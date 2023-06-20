09d398466371fb520a68a2cfa4dd29e3db4e74e3
KeKeZMen <maksimp.09.2004@gmail.com>
2023-06-21 01:32:13 +0300

    deleting tasks

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -1,7 +1,8 @@
 import { FC, DragEvent } from "react";
 
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, Button } from "@mui/material";
 
+import { useDeleteTaskMutation } from "../store/slices/tasksApi";
 import ITask from "../models/ITask";
 
 type PropsType = {
@@ -13,12 +14,16 @@ const Task: FC<PropsType> = ({ task }) => {
     e.dataTransfer.setData("cardInfo", JSON.stringify({ id: task.id, status: task.status }))
   };
 
+  const [deleteTask, {}] = useDeleteTaskMutation()
+  const handleDeleteTask = () => deleteTask({ taskId: task.id })
+
   return (
     <Card key={task.id} sx={{ mb: 3, minHeight: 120, width: 300 }} draggable onDragStart={dragStartHandler}>
       <CardContent>
         <Typography variant="h4">{task.title}</Typography>
         <Typography>{task.description}</Typography>
         <Typography>{task.final_date.toLocaleString()}</Typography>
+        <Button onClick={handleDeleteTask}>Удалить</Button>
       </CardContent>
     </Card>
   );
