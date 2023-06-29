7cce447bb8426e275cfac6ae0bfdf58de61148f4
Kekez <makeskex@yandex.ru>
2023-06-29 14:52:11 +0300

    ! for env and localhost

--- server/src/services/token-service.ts
+++ server/src/services/token-service.ts
@@ -1,15 +1,12 @@
-import jwt, { Secret } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 import IUser from "../models/IUser.js";
 import prisma from "./db-service.js";
 
 export default class TokenService {
   static generateTokens(userData: IUser) {
-    let secretAccess: Secret = "secret";
-    if (process.env.ACCESS) secretAccess = process.env.ACCESS;
-
-    let secretRefresh: Secret = "secret";
-    if (process.env.REFRESH) secretRefresh = process.env.REFRESH;
+    const secretAccess = process.env.ACCESS!;
+    const secretRefresh = process.env.REFRESH!;
 
     const accessToken = jwt.sign(userData, secretAccess, { expiresIn: "15m" });
     const refreshToken = jwt.sign(userData, secretRefresh, { expiresIn: "30d" });
@@ -21,9 +18,8 @@ export default class TokenService {
   }
 
   static verifyAccessToken(accessToken: string) {
-    if (!process.env.ACCESS) return null
-    let secretAccess: Secret = process.env.ACCESS;
-    
+    const secretAccess = process.env.ACCESS!;
+
     try {
       return jwt.verify(accessToken, secretAccess);
     } catch (error) {
@@ -32,8 +28,7 @@ export default class TokenService {
   }
 
   static verifyRefreshToken(refreshToken: string) {
-    if (!process.env.REFRESH) return null
-    let secretRefresh: Secret = process.env.REFRESH;
+    const secretRefresh = process.env.REFRESH!;
 
     try {
       return jwt.verify(refreshToken, secretRefresh);
@@ -48,9 +43,9 @@ export default class TokenService {
         refresh_token: refreshToken,
         user: {
           connect: {
-            id: userId
-          }
-        }
+            id: userId,
+          },
+        },
       },
     });
   }
@@ -58,8 +53,8 @@ export default class TokenService {
   static async removeRefreshToken(refreshToken: string) {
     return await prisma.token.delete({
       where: {
-        refresh_token: refreshToken
-      }
-    })
+        refresh_token: refreshToken,
+      },
+    });
   }
 }
